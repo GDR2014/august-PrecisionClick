419c2fa226dc2a6b104b74f1e2e0cc694e0b84c5
KHReenberg <khreenberg@gmail.com>
2014-08-24 22:15:25 +0200

    Adds functionality to the Main Menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using TargetClicker.UI;
+using UnityEngine;
+
+namespace TargetClicker
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        void Start()
+        {
+            UIManager.Instance.ShowLayout(UIManager.LAYOUT.MAIN_MENU);
+        }
+
+        public void StartNewGame()
+        {
+            UIManager.Instance.ShowLayout(UIManager.LAYOUT.GAME_HUD);
+        }
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,65 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace TargetClicker.UI
 {
-	public class UIManager : MonoBehaviour
-	{
+    public class UIManager
+    {
+        #region Singleton
+        private static UIManager _instance;
+        public static UIManager Instance
+        {
+            get
+            {
+                if( _instance == null ) _instance = new UIManager();
+                return _instance;
+            }
+        }
+        #endregion
 
-		void Start ()
-		{
+        public enum LAYOUT
+        {
+            MAIN_MENU,
+            GAME_HUD,
+        }
 
-		}
+        private readonly Canvas _uiCanvas;
+        private GameObject _currentLayout;
 
-		void Update ()
-		{
+        private Dictionary<LAYOUT, GameObject> _layouts; 
 
-		}
-	}
+        private UIManager()
+        {
+            _uiCanvas = GameObject.FindGameObjectWithTag("uiCanvas").GetComponent<Canvas>();
+            _currentLayout = initializeLayouts();
+        }
+
+        /// <summary>
+        /// Initializes the layouts map.
+        /// </summary>
+        /// <returns>The starting layout.</returns>
+        private GameObject initializeLayouts()
+        {
+            _layouts = new Dictionary<LAYOUT, GameObject>();
+            GameObject mainMenu = _uiCanvas.transform.FindChild( "pnlMainMenu" ).gameObject;
+            _layouts.Add(LAYOUT.MAIN_MENU, mainMenu);
+            GameObject gameHud = _uiCanvas.transform.FindChild( "pnlHud" ).gameObject;
+            _layouts.Add(LAYOUT.GAME_HUD, gameHud);
+            return mainMenu;
+        }
+
+        /// <summary>
+        /// Changes the UI layout.
+        /// </summary>
+        /// <param name="layoutType">Then new layout to show.</param>
+        public void ShowLayout(LAYOUT layoutType)
+        {
+            GameObject newLayout;
+            bool exists = _layouts.TryGetValue( layoutType, out newLayout );
+            if( !exists || newLayout == _currentLayout) return;
+            _currentLayout.SetActive( false );
+            _currentLayout = newLayout;
+            _currentLayout.SetActive(true);
+        }
+    }
 }
