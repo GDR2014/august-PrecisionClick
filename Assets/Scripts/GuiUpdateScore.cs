bad7a2d7a38437eeb15c65a118757a55a4e5922c
KHReenberg <khreenberg@gmail.com>
2014-08-26 23:58:43 +0200

    Adds sounds

---
+++ Assets/Scripts/GuiUpdateScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TargetClicker.UI
+{
+    [RequireComponent(typeof(Text))]
+    public class GuiUpdateScore : MonoBehaviour
+    {
+        private Text _text;
+
+        void Start()
+        {
+            _text = GetComponent<Text>();
+            ScoreManager.ScoreChangedEvent += OnScoreChanged;
+        }
+
+        private void OnScoreChanged( int previousscore )
+        {
+            _text.text = ScoreManager.Instance.CurrentScore + "";
+        }
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace TargetClicker
+{
+    public class SoundManager : MonoBehaviour
+    {
+        private const float BULLSEYE_THRESHOLD = 10;
+
+        [SerializeField] private AudioClip HitNormal;
+        [SerializeField] private AudioClip HitBullseye;
+
+        public float Volume { get; set; }
+        public bool Muted { get; set; }
+
+        private const string PREFKEY_VOLUME = "soundmanager volume";
+        private const string PREFKEY_MUTED = "soundmanager muted";
+
+        void Start()
+        {
+            Target.TargetClickedEvent += OnTargetClicked;
+            Volume = PlayerPrefs.GetFloat( PREFKEY_VOLUME, 1 );
+            Muted = PlayerPrefs.GetInt( PREFKEY_MUTED, 0 ) > 0;
+        }
+
+        private void OnTargetClicked( Target target, float percentageFromCenter )
+        {
+            if( Muted ) return;
+            bool isBullseye = percentageFromCenter < BULLSEYE_THRESHOLD;
+            AudioClip clip = isBullseye ? HitBullseye : HitNormal;
+            AudioSource.PlayClipAtPoint(clip, target.transform.position, Volume);
+        }
+    }
+}
